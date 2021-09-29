1cf12dba93bd344ae5a90951f899ecdb4e088cd7
Alexander Sokolov <1kdaye3aw@relay.firefox.com>
2021-09-29 09:39:13 +0530

    git-rebase 3

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,8 +3,13 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
 echo "\$@ Parameter #$count = $param"
 count=$(( $count + 1 ))
+=======
+    echo "Next parameter: $param"
+    count=$(( $count + 1 ))
+>>>>>>> 5c0c410... git-rebase 2
 done
 
 echo "====="
